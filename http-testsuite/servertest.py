1eeb6434cb664662a9248279dedc001857ca1c46
JohannesDev <jonny.bliha@gmail.com>
2020-12-21 11:14:23 +0100

    Add minimal server test

--- http-testsuite/servertest.py
+++ http-testsuite/servertest.py
@@ -1,6 +1,12 @@
 from httptest import HttpTest
 
 def main():
-    pass
+    h = HttpTest()
+
+    # These calls are not allowed and shold therefore fail
+    h.is_returncode("./server", 1)
+    h.is_returncode("./server -p 12 323", 1)
+    h.is_returncode("./server -m 1", 1)
+    h.print_result()
 
 main()
